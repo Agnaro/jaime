502ef28cb482d10c718376c1417f002a99d9048d
Eric Root <e70347@gmail.com>
2019-01-19 10:35:19 -0500

    Updates

---
+++ precache-manifest.5ecad60672d4c70a0bb2d7acca1bd31b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08d8735daebf6f18db3d",
+    "url": "/jaime/static/js/runtime~main.08d8735d.js"
+  },
+  {
+    "revision": "8dd863de74a6dec27ce3",
+    "url": "/jaime/static/js/main.8dd863de.chunk.js"
+  },
+  {
+    "revision": "c7056ef292710c546310",
+    "url": "/jaime/static/js/1.c7056ef2.chunk.js"
+  },
+  {
+    "revision": "8dd863de74a6dec27ce3",
+    "url": "/jaime/static/css/main.8a22544e.chunk.css"
+  },
+  {
+    "revision": "583ccb2ea24fbdaad91afb86d113d46a",
+    "url": "/jaime/index.html"
+  }
+];
